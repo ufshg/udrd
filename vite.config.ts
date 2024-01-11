1b3ce6a3eadb1389160ebb6b0a10329be9a88cd8
Kim, Hyeonseo <dodok8@gmail.com>
2024-01-11 09:54:15 +0000

    🔧 Update vite setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import checker from "vite-plugin-checker";
+import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    checker({
+      typescript: true,
+
+      eslint: {
+        lintCommand: "eslint src/",
+        dev: {
+          logLevel: ["warning"],
+        },
+      },
+      enableBuild: false,
+    }),
+  ],
+});
