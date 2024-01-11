0b88924f4d800dcd9d71f59edcca4e56f4222c75
Kim, Hyeonseo <dodok8@gmail.com>
2024-01-11 09:53:37 +0000

    🎨 Formatting ranges.d.ts

--- src/@types/ranges.d.ts
+++ src/@types/ranges.d.ts
@@ -1,8 +1,14 @@
-type Enumerate<N extends number, Acc extends number[] = []> = Acc['length'] extends N
-	? Acc[number]
-	: Enumerate<N, [...Acc, Acc['length']]>;
+type Enumerate<
+  N extends number,
+  Acc extends number[] = [],
+> = Acc["length"] extends N
+  ? Acc[number]
+  : Enumerate<N, [...Acc, Acc["length"]]>;
 
-export type IntRange<F extends number, T extends number> = Exclude<Enumerate<T>, Enumerate<F>>;
+export type IntRange<F extends number, T extends number> = Exclude<
+  Enumerate<T>,
+  Enumerate<F>
+>;
 
 export type BronzeRange = IntRange<0, 5>;
 export type SilverRange = IntRange<5, 10>;
@@ -11,4 +17,10 @@ export type PlatinumRange = IntRange<15, 20>;
 export type DiamondRange = IntRange<20, 25>;
 export type RubyRange = IntRange<25, 30>;
 
-export type TierRange = BronzeRange | SilverRange | GoldRange | PlatinumRange | DiamondRange | RubyRange;
+export type TierRange =
+  | BronzeRange
+  | SilverRange
+  | GoldRange
+  | PlatinumRange
+  | DiamondRange
+  | RubyRange;
