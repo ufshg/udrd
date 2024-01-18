d7a149ed5b036aa2e8d0c25adf67090b3252267a
Kim, Hyeonseo <dodok8@gmail.com>
2024-01-18 11:01:11 +0000

    💄 Apply Reset 2024 CSS

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./normalize.css";
 import "./App.css";
+import "./Reset2024.css";
 
 function App() {
   const [count, setCount] = useState(0);
