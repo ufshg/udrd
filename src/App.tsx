03f9ed87671bf1cef442d67250f07ae9d3948bd7
Kim, Hyeonseo <dodok8@gmail.com>
2024-02-03 07:22:28 +0000

    🎨 Add sort-imports rule

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { HistoryGraph } from "./Components/HistoryGraph";
 import reactLogo from "./assets/react.svg";
+import { useState } from "react";
 import viteLogo from "/vite.svg";
 import "./normalize.css";
 import "./App.css";
 import "./Reset2024.css";
-import { HistoryGraph } from "./Components/HistoryGraph";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -24,7 +24,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <HistoryGraph />
+        <HistoryGraph data={} />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
