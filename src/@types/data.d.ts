7a350bb6310f6e1ae046ddd9d2d9052e8f99baba
Kim, Hyeonseo <dodok8@gmail.com>
2024-02-03 08:00:41 +0000

    🏷️ Add UserSolvingHistory Type

--- src/@types/data.d.ts
+++ src/@types/data.d.ts
@@ -1,7 +1,10 @@
+import { TierRange } from "./tier";
+
 export type UserSolvingHistory = {
   user_id: number;
   solved: Array<{
     prob_id: number;
+    tier: TierRange;
     date: Date;
   }>;
 };
