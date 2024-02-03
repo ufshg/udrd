966a6e5e025617fc88cc81e488e607a1ececc794
Kim, Hyeonseo <dodok8@gmail.com>
2024-02-03 08:16:17 +0000

    🐛 Change return type of UserSolvingHistory Converter

--- src/utils/dataConversion.ts
+++ src/utils/dataConversion.ts
@@ -3,12 +3,10 @@ import type { UserSolvingHistory } from "src/@types/data.d.ts";
 
 export const convertUserSolvingHistoryToHistoryGraphData = (
   history: UserSolvingHistory
-): Pick<HistoryGraphProps, "data"> => {
-  return {
-    data: history.solved.map((solve) => ({
-      x: solve.date,
-      y: solve.tier, // Assuming prob_id is used to determine the tier
-      id: solve.prob_id,
-    })),
-  };
+): HistoryGraphProps["data"] => {
+  return history.solved.map((solve) => ({
+    x: solve.date,
+    y: solve.tier,
+    id: solve.prob_id,
+  }));
 };
